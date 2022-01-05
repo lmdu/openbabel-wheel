ad0f987667daab70d5673a31dd4589301bb1720f
Lianming Du <adullb@qq.com>
2022-01-05 21:11:56 +0800

    added installer

---
+++ openbabel_install.sh
@@ -0,0 +1,11 @@
+cmake --version
+
+wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
+tar xzvf openbabel-3-1-1.tar.gz
+cd openbabel-openbabel-3-1-1
+mkdir build
+cd build
+cmake ..
+make
+make install
+cd ../../
