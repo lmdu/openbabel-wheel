80ea686ec43ddbd3385344b449f2b582506d6892
Lianming Du <adullb@qq.com>
2022-01-06 16:34:03 +0800

    fixed download

--- openbabel_install.sh
+++ openbabel_install.sh
@@ -1,4 +1,4 @@
-curl -O http://zlib.net/zlib-1.2.11.tar.gz
+wget http://zlib.net/zlib-1.2.11.tar.gz
 tar xzf zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure
@@ -8,7 +8,7 @@ cd ..
 
 cmake --version
 
-curl -O https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
+wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
 tar xzf openbabel-3-1-1.tar.gz
 cd openbabel-openbabel-3-1-1
 mkdir build
