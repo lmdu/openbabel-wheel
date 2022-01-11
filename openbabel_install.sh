ccffa9fe3c28082139500a2dee6344ad97c9cc4d
Lianming Du <adullb@qq.com>
2022-01-11 13:36:05 +0800

    update

--- openbabel_install.sh
+++ openbabel_install.sh
@@ -8,12 +8,15 @@ cd ..
 
 cmake --version
 
-#wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
-#tar xzf openbabel-3-1-1.tar.gz
+wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
+tar xzf openbabel-3-1-1.tar.gz
 cd openbabel-openbabel-3-1-1
 mkdir build
 cd build
-cmake ..
+cmake .. -DRUN_SWIG=ON -DPYTHON_BINDINGS=ON
 make -j2
 make install
 cd ../../
+
+pwd
+ls -ll
