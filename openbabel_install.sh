60f87413829264ebc56c8c2a7c2fcbc9214f362f
Lianming Du <adullb@qq.com>
2022-01-18 07:16:55 +0800

    remove mac

--- openbabel_install.sh
+++ openbabel_install.sh
@@ -1,23 +1,18 @@
-#wget http://zlib.net/zlib-1.2.11.tar.gz
-#tar xzf zlib-1.2.11.tar.gz
-#cd zlib-1.2.11
-#./configure
-#make
-#make install
-#cd ..
+wget http://zlib.net/zlib-1.2.11.tar.gz
+tar xzf zlib-1.2.11.tar.gz
+cd zlib-1.2.11
+./configure
+make
+make install
+cd ..
 
-#wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
-#tar xzf openbabel-3-1-1.tar.gz
-#cd openbabel-openbabel-3-1-1
-brew search gcc
-brew install gcc@8
-gcc --version
-cxx --version
+wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
+tar xzf openbabel-3-1-1.tar.gz
+cd openbabel-openbabel-3-1-1
 rm -rf build
 mkdir build
 cd build
 cmake .. -DRUN_SWIG=ON -DPYTHON_BINDINGS=ON
 make -j4
 make install
-#cd ../../
-cd ..
+cd ../../
