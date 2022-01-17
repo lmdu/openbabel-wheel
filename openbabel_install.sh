c126f31da572fbf179e15dd4a0b326b4a1338b20
Lianming Du <adullb@qq.com>
2022-01-17 21:37:12 +0800

    update

--- openbabel_install.sh
+++ openbabel_install.sh
@@ -1,27 +1,19 @@
-wget http://zlib.net/zlib-1.2.11.tar.gz
-tar xzf zlib-1.2.11.tar.gz
-cd zlib-1.2.11
-./configure
-make
-make install
-cd ..
+#wget http://zlib.net/zlib-1.2.11.tar.gz
+#tar xzf zlib-1.2.11.tar.gz
+#cd zlib-1.2.11
+#./configure
+#make
+#make install
+#cd ..
 
-cmake --version
-python --version
-python3 --version
-pip --version
-pip3 --version
-which python
-which python3
-ls -l  /tmp/cibw_bin/
-
-wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
-tar xzf openbabel-3-1-1.tar.gz
-cd openbabel-openbabel-3-1-1
+#wget https://github.com/openbabel/openbabel/archive/refs/tags/openbabel-3-1-1.tar.gz
+#tar xzf openbabel-3-1-1.tar.gz
+#cd openbabel-openbabel-3-1-1
 rm -rf build
 mkdir build
 cd build
 cmake .. -DRUN_SWIG=ON -DPYTHON_BINDINGS=ON
 make -j2
 make install
-cd ../../
+#cd ../../
+cd ..
