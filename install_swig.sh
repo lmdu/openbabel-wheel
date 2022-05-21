fe90a642cd564636429a0be67f757bd0c86beb81
Lianming Du <adullb@qq.com>
2022-05-22 00:38:03 +0800

    new

--- install_swig.sh
+++ install_swig.sh
@@ -1,5 +1,15 @@
+yum install -y wget
+
 tar xzvf swig-4.0.2.tar.gz
 cd swig-4.0.2
+./configure
+make
+make install
+
+wget https://zlib.net/zlib-1.2.12.tar.gz
+
+tar xzvf zlib-1.2.12.tar.gz
+cd zlib-1.2.12
 ./configure
 make
 make install
