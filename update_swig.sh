5a4e4288bec01f37d026faab08e21c65d7756514
Lianming Du <adullb@qq.com>
2022-05-24 15:13:19 +0800

    update

--- update_swig.sh
+++
@@ -1,7 +0,0 @@
-yum install -y wget
-wget –no-check-certificate https://sourceforge.net/projects/swig/files/swig/swig-4.0.2/swig-4.0.2.tar.gz
-tar xzvf swig-4.0.2.tar.gz
-cd swig-4.0.2
-./configure
-make
-make install
