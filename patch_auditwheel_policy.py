01cf903be0bad4823afae3e12cda2df8446ebd92
Lianming Du <adullb@qq.com>
2024-06-17 22:48:15 +0800

    fixed aw policy

---
+++ patch_auditwheel_policy.py
@@ -0,0 +1,12 @@
+import json
+
+policy_file = "/opt/_internal/pipx/venvs/auditwheel/lib/python3.10/site-packages/auditwheel/policy/manylinux-policy.json"
+
+with open(policy_file) as fh:
+    policies = json.load(fh)
+
+for p in policies:
+    p['lib_whitelist'].append('libxcb.so.1')
+
+with open(policy_file, 'w') as fw:
+    json.dump(data, fh)
